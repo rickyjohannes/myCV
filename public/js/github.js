ac05d6dd7914f460eb783d68384f3c6fe0c3866c
rickyjohannes <112993455+rickyjohannes@users.noreply.github.com>
2022-09-07 08:26:12 +0700

    Add files via upload

---
+++ public/js/github.js
@@ -0,0 +1,23 @@
+var request = new XMLHttpRequest()
+
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET', 'https://api.github.com/users/baskorocr/repos', true)
+
+request.onload = function () {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+
+  var statusHTML = '';
+  $.each(data, function(i, status) {
+    statusHTML += '<tr>';
+    
+    statusHTML += '<td>' + status.name + '</td>';
+    statusHTML += '<td>' + status.html_url + '</td>';
+    statusHTML += '<td>' + status.language + '</td>';
+    statusHTML += '</tr>';
+  });
+  $('tbody').html(statusHTML);
+}
+
+// Send request
+request.send();
